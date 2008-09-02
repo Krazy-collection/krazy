36aad4a9e86203427e13943bd88fe03331b053e1
winterz <winterz@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-09-02 20:27:05 +0000

    cleaning
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@856419 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- install.sh
+++ install.sh
@@ -43,13 +43,13 @@ make realclean
 #create a symlink to the perl modules so we can find them
 V=`perl -e 'printf "%vd", $^V'`
 if ( test -d $TOP/lib/perl5/site_perl ) then
-  (cd $TOP/lib; rm -f Krazy; rm -f Krazy; ln -s $TOP/lib/perl5/site_perl/$V/Krazy Krazy)
+  (cd $TOP/lib; rm -f Krazy; ln -s $TOP/lib/perl5/site_perl/$V/Krazy Krazy)
 else
   if ( test -d $TOP/lib64/perl5/site_perl ) then
-    (cd $TOP/lib; rm -f Krazy; rm -f Krazy; ln -s $TOP/lib64/perl5/site_perl/$V/Krazy Krazy)
+    (cd $TOP/lib; rm -f Krazy; ln -s $TOP/lib64/perl5/site_perl/$V/Krazy Krazy)
   else
     if ( test -d $TOP/share/perl ) then
-	(cd $TOP/lib; rm -f Krazy; rm -f Krazy; ln -s $TOP/share/perl/$V/Krazy Krazy)
+	(cd $TOP/lib; rm -f Krazy; ln -s $TOP/share/perl/$V/Krazy Krazy)
     else
 	echo
 	echo "==================================================================="
