74c78958e6c9d3d9c8b7c0d254b7575758405bf9
winterz <winterz@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2008-12-11 23:56:19 +0000

    check for replace("x",QString())
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@895922 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- testdata/foo.cpp
+++ testdata/foo.cpp
@@ -16,6 +16,9 @@ str += "allen" + "d" + "winter"; //doublequote_chars
 str += "allen"; //ok
 str = "al" + "llen"; //ok
 str = "a" + "bc" + "d"; //doublequote_chars
+str.replace("abc", "" ); //doublequote_chars
+str.replace("abc", QString( )) //doublequote_chars
+str.remove("a"); //doublequote_chars
 
 //contractions
 str = i18n( "don't" ); // contractions
