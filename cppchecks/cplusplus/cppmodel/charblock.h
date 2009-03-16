c1244ceefd8bba4b952d2c6cb12024d9f8f405ab
bbroeksema <bbroeksema@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-03-16 15:14:08 +0000

    I've been a bit nasty, although not intentional, on Roberto/Nokia with respect to licensing of the files. With this commit I change the licenses of the files to LGPL and add of course Roberto as (c) as he did most of the work in here anyway.
    Excuse moi!
    
    
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@940085 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- cppchecks/cplusplus/cppmodel/charblock.h
+++ cppchecks/cplusplus/cppmodel/charblock.h
@@ -1,22 +1,19 @@
-// Copyright (c) 2009 Bertjan Broeksema <b.broeksema@home.nl>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+/*
+   This file or code within this file was originally part of Qt Creator.
+
+   Copyright (c) 2008 Roberto Raggi <roberto.raggi@gmail.com>
+   Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+   Copyright (c) 2009 Bertjan Broeksema <b.broeksema@kdemail.net>
+
+   GNU Lesser General Public License Usage
+
+   This file may be used under the terms of the GNU Lesser
+   General Public License version 2.1 as published by the Free Software
+   Foundation and appearing in the file LICENSE.LGPL included in the
+   packaging of this file.  Please review the following information to
+   ensure the GNU Lesser General Public License version 2.1 requirements
+   will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+*/
 #ifndef CPLUSPLUS_CPPMODEL_CHARBLOCK_H
 #define CPLUSPLUS_CPPMODEL_CHARBLOCK_H
 
--- cppchecks/cplusplus/cppmodel/document.h
+++ cppchecks/cplusplus/cppmodel/document.h
@@ -1,20 +1,18 @@
 /*
-    Copyright (C) 2009 Bertjan Broeksema <b.broeksema@kdemail.net>
+   This file or code within this file was originally part of Qt Creator.
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+   Copyright (c) 2008 Roberto Raggi <roberto.raggi@gmail.com>
+   Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+   Copyright (c) 2009 Bertjan Broeksema <b.broeksema@kdemail.net>
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+   GNU Lesser General Public License Usage
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+   This file may be used under the terms of the GNU Lesser
+   General Public License version 2.1 as published by the Free Software
+   Foundation and appearing in the file LICENSE.LGPL included in the
+   packaging of this file.  Please review the following information to
+   ensure the GNU Lesser General Public License version 2.1 requirements
+   will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 */
 #ifndef CPLUSPLUS_CPPMODEL_DOCUMENT_H
 #define CPLUSPLUS_CPPMODEL_DOCUMENT_H
