cb99539bb7c8357685161b0fbc0a06e59570dd64
bbroeksema <bbroeksema@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-03-16 12:15:06 +0000

    Fix licenses.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@940029 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- cppchecks/gui/parseresultwidget.h
+++ cppchecks/gui/parseresultwidget.h
@@ -1,3 +1,24 @@
+/*
+    Copyright (C) 2009 Bertjan Broeksema <b.broeksema@kdemail.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+#ifndef PARSE_RESULT_WIDGET_H
+#define PARSE_RESULT_WIDGET_H
+
 #include <QtGui/QWidget>
 
 #include <cppmodel/document.h>
@@ -45,3 +66,5 @@ class ParseResultWidget : public QWidget
     Ui::ParseResultWidget             *m_ui;
 	CPlusPlus::Scope                  *m_globals;
 };
+
+#endif // PARSE_RESULT_WIDGET_H
