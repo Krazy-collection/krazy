7016ba17840842df1441b565bd255050dc8d030a
bbroeksema <bbroeksema@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-03-23 14:20:12 +0000

    Add a directory which will contain some simple source files to check the parser.
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@943217 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ cppchecks/tests/simple-test-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  string s = "een string";
+  s += " meer tekst";
+  cout << s << endl;
+  
+  return 0;
+}
