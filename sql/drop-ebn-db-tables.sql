3175c1a895443c2fb4a8bf857ab1530ab9b71fba
bbroeksema <bbroeksema@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-04-24 18:04:11 +0000

    Move the sql code to the new componentchecker framework as discussed with winterz.
    Add a README with a first discription of the new framework.
    
    
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@958802 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- sql/drop-ebn-db-tables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE tools CASCADE;
-DROP TABLE plugins CASCADE;
-DROP TABLE tool_plugins CASCADE;
-DROP TABLE component_check CASCADE;
-DROP TABLE tool_run CASCADE;
-DROP TABLE tool_run_results CASCADE;
-DROP TABLE tool_run_result_issues CASCADE;
