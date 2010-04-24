794ecc9636348d851b5c5201c1c80185a2924e70
winterz <winterz@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2010-04-24 11:45:37 +0000

    possible fix for installing on Fedora13. BUG: 232974
    Michael, if this still doesn't work I need you to send me the long listing
    of your ~/krazy2 directory as it looks after the install.sh quits.
    
    
    git-svn-id: svn+ssh://svn.kde.org/home/kde/trunk/quality/krazy2@1118314 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- install.sh
+++ install.sh
@@ -81,18 +81,22 @@ else
   if ( test -d $TOP/share/perl ) then
     (cd $TOP/lib; rm -f Krazy; ln -s $TOP/share/perl/$V/Krazy Krazy)
   else
-    if ( test -d $TOP/lib/perl5/site_perl ) then
-      (cd $TOP/lib; rm -f Krazy; ln -s $TOP/lib/perl5/site_perl/$V/Krazy Krazy)
+    if ( test -d $TOP/share/perl5 ) then
+      (cd $TOP/lib; rm -f Krazy; ln -s $TOP/share/perl5/$V/Krazy Krazy)
     else
-      if ( test -d $TOP/lib64/perl5/site_perl ) then
-        (cd $TOP/lib; rm -f Krazy; ln -s $TOP/lib64/perl5/site_perl/$V/Krazy Krazy)
+      if ( test -d $TOP/lib/perl5/site_perl ) then
+        (cd $TOP/lib; rm -f Krazy; ln -s $TOP/lib/perl5/site_perl/$V/Krazy Krazy)
       else
-        echo
-	echo "==================================================================="
-	echo "Unknown perl installation issue encountered. Aborting installation."
-	echo "Please contact winter@kde.org about this."
-	echo "==================================================================="
-	exit 1
+        if ( test -d $TOP/lib64/perl5/site_perl ) then
+          (cd $TOP/lib; rm -f Krazy; ln -s $TOP/lib64/perl5/site_perl/$V/Krazy Krazy)
+        else
+          echo
+	  echo "==================================================================="
+	  echo "Unknown perl installation issue encountered. Aborting installation."
+	  echo "Please contact winter@kde.org about this."
+	  echo "==================================================================="
+	  exit 1
+        fi
       fi
     fi
   fi
