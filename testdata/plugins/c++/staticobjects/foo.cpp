e87d32a0ae6bc649e4f71794008ae2323ca6a997
Allen Winter <allen.winter@kdab.com>
2011-05-22 07:24:08 -0400

    Allow assigning an enum (or enum + constant).  fixes a false positive Patch by David Faure.

--- testdata/plugins/c++/staticobjects/foo.cpp
+++ testdata/plugins/c++/staticobjects/foo.cpp
@@ -5,7 +5,10 @@ static int foo = myInitializer(); // myInitializer() might not be called
 static int foo(); //ok func decl
 static int foo(const QString &); //ok func decl
 
+static const int VariableTypeProperty = QTextFormat::UserProperty + 246; //ok
+
 static const int i = 42;  //ok
+static const int i2 = 42 + 2;  //ok
 static const int ii[3] = {1, 2, 3}; //ok
 static const char myString[] = "hello"; //ok
 static const MyStruct s = {3, 4.4, "hello"}; //ok
