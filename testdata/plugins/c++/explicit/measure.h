19cee02eb7f91ccccc2649ed2dfedaeb1db6e6f2
Allen Winter <allen.winter@kdab.com>
2012-01-11 17:29:40 -0500

    more test data

---
+++ testdata/plugins/c++/explicit/measure.h
@@ -0,0 +1,15 @@
+class Measure
+{
+public:
+    /*implicit*/ Measure( qreal value,
+                          int mode = 1,
+                          int orientation = 2 );
+
+    Measure( qreal value, int mode = 1, int orientation = 2 );
+};
+
+class GlobalMeasureScaling
+{
+public:
+    GlobalMeasureScaling();
+};
