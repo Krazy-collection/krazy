a7b927874220e0ec3e723258c2f4fa9525e35436
Allen Winter <allen.winter@kdab.com>
2013-06-22 16:46:51 -0400

    first version of a Python style checker

---
+++ testdata/extras/python/style/t1.py
@@ -0,0 +1,27 @@
+
+#
+
+
+import sys
+
+"""
+comment goes
+here
+"""
+
+trail_whitespace=True
+
+if trail_whitespace: 
+  trail_whitespace = False
+
+
+  
+"""
+this is some documentation this program.
+this program is good at absolutely nothing at all but it
+does help us test the Python style checker program. This line is longer than 100 chars and should issue.
+so there
+"""
+very_long_variable_name_for_testing_long_lines_for_the_python_style_checker = x + y / z # and a comment 
+
+
